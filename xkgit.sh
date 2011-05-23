d69e33690c065248bb11f898e67b0d47786deff3
Jesse L. Zamora <xtremekforever@gmail.com>
2011-05-23 11:54:53 -0400

    Add basic skeleton

--- xkgit.sh
+++ xkgit.sh
@@ -1,1 +1,11 @@
- 
+#/bin/bash
+
+cmd=$1
+
+if   [ "$cmd" = "setup" ]; then
+
+elif [ "$cmd" = ""      ]; then
+  echo "Please specify a command for this script to run!"
+  echo "Example: "
+  echo " $0 pull"
+fi
